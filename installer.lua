d29a889c9ce87424ee1f731ea8c724064c8afc98
ExplosiveHDYT <82344578+ExplosiveHDYT@users.noreply.github.com>
2025-04-27 21:58:37 -0400

    Create installer.lua

---
+++ installer.lua
@@ -0,0 +1,24 @@
+-- installer.lua
+local url = {
+  storageInterface = "https://raw.githubusercontent.com/ExplosiveHDYT/stockMarket/refs/heads/main/storageInterface.lua",
+  tickers = "https://raw.githubusercontent.com/ExplosiveHDYT/stockMarket/refs/heads/main/ticker.lua",
+  transmitter = "https://raw.githubusercontent.com/ExplosiveHDYT/stockMarket/refs/heads/main/transmitter.lua",
+}
+
+for name, link in pairs(url) do
+  print(name)
+end
+
+print("insert package name")
+local name = read()
+
+print("Install " .. name .. " as startup.lua? (y/n)")
+if read() == "y" then
+  if fs.exists("startup.lua") then
+    fs.delete("startup.lua")
+  end
+  shell.run("wget", url[name], "startup.lua")
+  print("Done.")
+else
+  print("Cancelled.")
+end
